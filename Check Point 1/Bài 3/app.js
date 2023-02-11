499e06a30ed65e5c4eddc988479ed26fc805a4e6
DoHoangKhai <cacahoangkhai@gmail.com>
2023-02-11 19:53:00 +0700

    Finish

--- Check Point 1/Bài 3/app.js
+++ Check Point 1/Bài 3/app.js
@@ -1,2 +1,11 @@
+username = document.getElementById("username").value;
+password = document.getElementById("pass").value;
+dob = document.getElementById("dob").value;
+gender = document.getElementById("genders").value;
 
+let userInfo = {}
+
+function register(){
+    console.log(username)
+}
 

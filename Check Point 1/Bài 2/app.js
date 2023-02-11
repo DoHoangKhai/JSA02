5e69889e9ce1ef3f676bbbc91e42c0008c02c349
DoHoangKhai <cacahoangkhai@gmail.com>
2023-02-11 17:59:09 +0700

    bai 2

--- Check Point 1/Bài 2/app.js
+++ Check Point 1/Bài 2/app.js
@@ -1,12 +1,27 @@
-let triangle = ''
 
-window.addEventListener('load', function(){
-    let n = prompt('Enter Triangle height')
+let n = prompt('Enter Triangle height (Integer whole number)');
+let triangle = '';
 
-    for (i = 0; i < n; i++){
-        triangle += '*';
+for (let i = 1; i <= n; i++) {
+	for(let j = 1; j <= n-i; j++){
+		triangle += '&nbsp&nbsp';
+	}
+	for(let k = 0; k < 2*i-1; k++){
+
+        if(i == 1 || i == n){
+            triangle += '*'
+        }
+        else{
+            if(k == 0 || k == 2*i-2){
+                triangle += '*'
+            }else{
+                triangle += '&nbsp&nbsp';
+            }
+        }
     }
 
-    document.getElementById('out') = triangle
-})
+    triangle += '<br>'
+}
 
+document.getElementById('out').innerHTML = triangle
+console.log(triangle)
